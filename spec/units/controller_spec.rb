91eca1f5a7669f8748b3350413e48a1a48ee4314
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-13 17:13:46 +0100

    [Testing] Test if controller deposit method creates new transaction

---
+++ spec/units/controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'controller.rb'
+
+describe Controller do
+  let(:account) { double :account }
+  let(:transaction) { double :transaction }
+  controller = Controller.new(account, transaction)
+
+  describe '#deposit' do
+    it 'creates a new transaction' do
+      controller.deposit(100)
+      expect(transaction).to have_received(:new)
+    end
+  end
+end
