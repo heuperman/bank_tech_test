e5c280c46eb252c499dba9890fb688bafbdc9d78
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-13 11:39:30 +0100

    [Feature] Allow user to make deposits that update balance

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require 'account.rb'
+
 describe Account do
   let(:account) { Account.new }
 
   describe '#deposit' do
     it 'adds the given amount to account balance' do
-      expect(account.deposit(100)).to change { account.balance }.by(100)
+      expect{ account.deposit(100) }.to change { account.balance }.by(100)
     end
   end
 end
