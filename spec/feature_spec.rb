f544009d93cfc7782f5f1150bdd466048c75b923
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-13 11:41:55 +0100

    [Testing] Add test for withdrawals

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -7,7 +7,13 @@ describe Account do
 
   describe '#deposit' do
     it 'adds the given amount to account balance' do
-      expect{ account.deposit(100) }.to change { account.balance }.by(100)
+      expect { account.deposit(100) }.to change { account.balance }.by(100)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'reduces the account balance by the given amount' do
+      expect { account.withdraw(100) }.to change { account.balance }.by(-100)
     end
   end
 end
