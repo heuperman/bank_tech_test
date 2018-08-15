7fa4c888b7cabd9d4315c19c2666b0cedd87d1c8
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-15 11:33:47 +0100

    [Testing] Add feature test for withdrawing money

--- spec/features/withdraw_spec.rb
+++ spec/features/withdraw_spec.rb
@@ -2,14 +2,14 @@
 
 require 'account_controller.rb'
 
-describe 'depositing money' do
+describe 'withdrawing money' do
   let(:account_balance) { AccountBalance.new }
   let(:account_controller) do
     AccountController.new(account_balance: account_balance)
   end
 
-  it 'user can make a deposit into their account' do
-    account_controller.deposit(100.00)
-    expect(account_balance.balance).to be(100.00)
+  it 'user can make a withdrawal from their account' do
+    account_controller.withdraw(100.00)
+    expect(account_balance.balance).to be(-100.00)
   end
 end
